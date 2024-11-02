bd38c360608c8829c605ac917bf3e9a6e2af78ba
aadishah-projects <adityakshah69@gmail.com>
2024-11-02 16:16:25 +0545

    rendered a html to server

--- home/views.py
+++ home/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse ("<h1>Hello World, My name is Aditya Shah.</h1>")
+    return render(request, "index.html")
 
 def success_page(request):
     return HttpResponse ("This is a success page")
