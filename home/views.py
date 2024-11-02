61d8bfb39c9b6cef2d57ca25331494efdad13b3e
aadishah-projects <adityakshah69@gmail.com>
2024-11-02 16:24:59 +0545

    moved index html

--- home/views.py
+++ home/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "index.html")
+    return render(request, "home/index.html")
 
 def success_page(request):
     return HttpResponse ("This is a success page")
