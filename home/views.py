fbe54da5760e8030aa4b8f3326e4b2775a50f1d3
aadishah-projects <adityakshah69@gmail.com>
2024-11-03 20:19:09 +0545

    Template Engine : Printing a List from backend

--- home/views.py
+++ home/views.py
@@ -3,8 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+
+# Template Engine Transfering list to the index.html by django
 def home(request):
-    return render(request, "home/index.html")
+
+    people = [
+    {'name': 'Aditya Shah', 'age': 19},
+    {'name': 'Sara Ali', 'age': 24},
+    {'name': 'John Doe', 'age': 30},
+    {'name': 'Emily Zhang', 'age': 27},
+    {'name': 'Michael Brown', 'age': 35},
+    {'name': 'Linda Kim', 'age': 22},
+    {'name': 'Carlos Rivera', 'age': 28},
+]
+
+    return render(request, "home/index.html", context = {'peoples' : people} )
 
 def success_page(request):
     return HttpResponse ("This is a success page")
