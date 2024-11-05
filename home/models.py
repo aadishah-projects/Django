005682d328b930d019007dde8100fdbe63688c75
aadishah-projects <adityakshah69@gmail.com>
2024-11-05 19:55:03 +0545

    Model and Migrations

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+#Understanding models
+# passing Model class from model
+class Student(models.Model):
+    # id = models.AutoField()
+    name =  models.CharField(max_length = 100)
+    age = models.IntegerField(default= 18)
+    email = models.EmailField()
+    address = models.TextField()
+    # image = models.ImageField()
+    certificate = models.FileField()
